700b96f96627ce8605a64b8c81949f6debeaac53
mejia-dev <aaron.mejia8484@gmail.com>
2024-08-16 18:24:29 -0700

    Add first version of injected script

---
+++ src/scripts/inject.js
@@ -0,0 +1,37 @@
+document.addEventListener("MessengerBadges-Initialize", () => {
+  function doTheThing() {
+
+    const chatElement = document.querySelector("a[aria-label*='Chats']");
+
+    function setUnreadBadge(textToParse) {
+      const regex = /\d+/g;
+      const resultNumber = parseInt(textToParse.match(regex));
+      navigator.setAppBadge(resultNumber);
+    }
+
+    function checkForUnreadMessages() {
+      if (chatElement.textContent != "") {
+        setUnreadBadge(chatElement.textContent);
+      } else {
+        navigator.clearAppBadge();
+      }
+    }
+
+  
+  checkForUnreadMessages();
+
+  const observer = new MutationObserver((mutations) => {
+    for (let mutation of mutations) {
+      checkForUnreadMessages();
+    }
+  });
+
+  observer.observe(chatElement, {
+    characterData: true,
+    childList: true,
+    subtree: true,
+  });
+}
+
+setTimeout(doTheThing, 4000);
+});
