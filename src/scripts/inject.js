175a19da34db17ae0ff49ef19f35c9098768392c
mejia-dev <aaron.mejia8484@gmail.com>
2024-09-07 19:12:18 -0700

    Change badging functionality to persist even when Messenger clears the badges. Keeping old functionality commented out for now until confirmed by week of testing.

--- src/scripts/inject.js
+++ src/scripts/inject.js
@@ -1,3 +1,42 @@
+// Good and working!!
+// document.addEventListener("MPWAI-StartBadges", () => {
+//   function addBadges() {
+//     const chatElement = document.querySelector("a[aria-label*='Chats']");
+
+//     function setUnreadBadge(textToParse) {
+//       const regex = /\d+/g;
+//       const resultNumber = parseInt(textToParse.match(regex));
+//       navigator.setAppBadge(resultNumber);
+//     }
+
+//     function checkForUnreadMessages() {
+//       if (chatElement != null) {
+//         if (chatElement.textContent != "") {
+//           setUnreadBadge(chatElement.textContent);
+//         } else {
+//           navigator.clearAppBadge();
+//         }
+//       }
+//     }
+
+//     checkForUnreadMessages();
+
+//     const observer = new MutationObserver((mutations) => {
+//       for (let mutation of mutations) {
+//         checkForUnreadMessages();
+//       }
+//     });
+
+//     observer.observe(chatElement, {
+//       characterData: true,
+//       childList: true,
+//       subtree: true,
+//     });
+//   }
+
+//   setTimeout(addBadges, 4000);
+// });
+
 document.addEventListener("MPWAI-StartBadges", () => {
   function addBadges() {
     const chatElement = document.querySelector("a[aria-label*='Chats']");
@@ -19,23 +58,12 @@ document.addEventListener("MPWAI-StartBadges", () => {
     }
 
     checkForUnreadMessages();
-
-    const observer = new MutationObserver((mutations) => {
-      for (let mutation of mutations) {
-        checkForUnreadMessages();
-      }
-    });
-
-    observer.observe(chatElement, {
-      characterData: true,
-      childList: true,
-      subtree: true,
-    });
   }
 
-  setTimeout(addBadges, 4000);
+  window.setInterval(function(){
+    addBadges();
+  }, 1000);
 });
-
 
 document.addEventListener('keydown', function (event) {
   if (event.key === 'Escape' && !document.querySelector("*[aria-label='Media viewer']")) {
