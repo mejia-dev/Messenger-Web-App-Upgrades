4f582c922122579f542d1e256f148a03df40fb57
mejia-dev <aaron.mejia8484@gmail.com>
2024-09-24 23:14:01 -0700

    Add smooth loading (fixes toggle button animations playing on each launch)

--- src/mwau-popup/src/App.tsx
+++ src/mwau-popup/src/App.tsx
@@ -11,14 +11,15 @@ export default function App(): JSX.Element {
     unreadBadges: boolean
   }
 
-  const tempSettingsList: SettingsObject = {
+  const defaultSettingsList: SettingsObject = {
     escCloseChat: true,
     unreadBadges: true,
   }
 
   const [audioSrc, setAudioSrc] = useState<string>("");
+  const [loadingSettings, setLoadingSettings] = useState<boolean>(true);
   const [statusMessage, setStatusMessage] = useState<string>("");
-  const [settingsList, setSettingsList] = useState<SettingsObject>(tempSettingsList);
+  const [settingsList, setSettingsList] = useState<SettingsObject>(defaultSettingsList);
 
 
 
@@ -86,6 +87,7 @@ export default function App(): JSX.Element {
       if (result.settings) {
         setSettingsList(result.settings);
       }
+      setLoadingSettings(false);
     });
   }
 
@@ -111,27 +113,32 @@ export default function App(): JSX.Element {
         </p>
         <p><span id="message">{statusMessage}</span></p>
       </div>
+
       <div className="contentCard">
         <h4 id='settingsHeader'>Settings</h4>
-        <br />
+        {loadingSettings ? (<p>Loading...</p>) : (
+          <>
+            <br />
 
-        <SettingsItem
-          title="Escape to Close"
-          desc="Press the escape key to close the current chat."
-          value={settingsList.escCloseChat}
-          onChangeFunc={() => toggleSetting('escCloseChat')}
-          svgElement={(<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="bi bi-x-circle-fill" viewBox="0 0 16 16"><path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293z" /></svg>)}
-        />
+            <SettingsItem
+              title="Escape to Close"
+              desc="Press the escape key to close the current chat."
+              value={settingsList.escCloseChat}
+              onChangeFunc={() => toggleSetting('escCloseChat')}
+              svgElement={(<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="bi bi-x-circle-fill" viewBox="0 0 16 16"><path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293z" /></svg>)}
+            />
 
-        <SettingsItem
-          title="PWA Unread Badges"
-          desc="Display badges on the app if installed as a PWA."
-          value={settingsList.unreadBadges}
-          onChangeFunc={() => toggleSetting('unreadBadges')}
-          svgElement={(<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="bi bi-app-indicator" viewBox="0 0 16 16"><path d="M5.5 2A3.5 3.5 0 0 0 2 5.5v5A3.5 3.5 0 0 0 5.5 14h5a3.5 3.5 0 0 0 3.5-3.5V8a.5.5 0 0 1 1 0v2.5a4.5 4.5 0 0 1-4.5 4.5h-5A4.5 4.5 0 0 1 1 10.5v-5A4.5 4.5 0 0 1 5.5 1H8a.5.5 0 0 1 0 1z" /><path d="M16 3a3 3 0 1 1-6 0 3 3 0 0 1 6 0" /></svg>)}
-        />
-
+            <SettingsItem
+              title="PWA Unread Badges"
+              desc="Display badges on the app if installed as a PWA."
+              value={settingsList.unreadBadges}
+              onChangeFunc={() => toggleSetting('unreadBadges')}
+              svgElement={(<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="bi bi-app-indicator" viewBox="0 0 16 16"><path d="M5.5 2A3.5 3.5 0 0 0 2 5.5v5A3.5 3.5 0 0 0 5.5 14h5a3.5 3.5 0 0 0 3.5-3.5V8a.5.5 0 0 1 1 0v2.5a4.5 4.5 0 0 1-4.5 4.5h-5A4.5 4.5 0 0 1 1 10.5v-5A4.5 4.5 0 0 1 5.5 1H8a.5.5 0 0 1 0 1z" /><path d="M16 3a3 3 0 1 1-6 0 3 3 0 0 1 6 0" /></svg>)}
+            />
+          </>
+        )}
       </div>
+
     </>
   )
 }
