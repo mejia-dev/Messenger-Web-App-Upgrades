f03746b15151977736620558cdacdfc8b000e79e
mejia-dev <aaron.mejia8484@gmail.com>
2024-09-30 22:07:17 -0700

    Modify injector to send custom events based on which settings are enabled

--- src/scripts/injector.js
+++ src/scripts/injector.js
@@ -5,10 +5,11 @@
   script.setAttribute("type", "text/javascript");
   script.onload = function () {
     this.remove();
-    document.dispatchEvent(new CustomEvent("MWAU-StartBadges"));
     chrome.storage.local.get(["uploadedAudio", "settings"], function (result) {
-      if (result.uploadedAudio && result.settings) {
-        if (result.settings.customNotificationSound) document.dispatchEvent(new CustomEvent("MWAU-CustomSound", { detail: result.uploadedAudio }));
+      if (result.settings) {
+        if (result.settings.customNotificationSound && result.uploadedAudio) document.dispatchEvent(new CustomEvent("MWAU-CustomSound", { detail: result.uploadedAudio }));
+        if (result.settings.unreadBadges) document.dispatchEvent(new CustomEvent("MWAU-StartBadges"));
+        if (result.settings.escCloseChat) document.dispatchEvent(new CustomEvent("MWAU-StartEscCloseChat"));
       }
     });
   }
