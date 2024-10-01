fbaca5e0668ac088c518c421e217bc7f3b882f79
mejia-dev <aaron.mejia8484@gmail.com>
2024-09-30 20:31:52 -0700

    Separate SoundSettings data saving functionality from App

--- src/mwau-popup/src/App.tsx
+++ src/mwau-popup/src/App.tsx
@@ -18,54 +18,8 @@ export default function App(): JSX.Element {
     unreadBadges: true,
   }
 
-  const [audioSrc, setAudioSrc] = useState<string>("");
   const [loadingSettings, setLoadingSettings] = useState<boolean>(true);
-  const [statusMessage, setStatusMessage] = useState<string>("");
   const [settingsList, setSettingsList] = useState<SettingsObject>(defaultSettingsList);
-
-
-
-  async function handleAudioUpload(): Promise<void> {
-    try {
-      const audioFileInput = document.getElementById("audioFileUpload") as HTMLInputElement;
-      const fileUpload: File = audioFileInput.files![0];
-      const reader = new FileReader();
-      reader.onload = function (e: ProgressEvent<FileReader>) {
-        const audioDataURL = e.target!.result as string;
-        chromeStorage.set({ uploadedAudio: audioDataURL },
-          () => {
-            setStatusMessage("Successfully uploaded file. Please refresh Messenger to activate the custom sound.");
-            loadAudio();
-          }
-        );
-      };
-      reader.readAsDataURL(fileUpload);
-    }
-    catch (error: unknown) {
-      if (error instanceof Error) setStatusMessage(error.message);
-    }
-  }
-
-  interface StoredData {
-    uploadedAudio?: string;
-  }
-
-  async function loadAudio(): Promise<void> {
-    chromeStorage.get(["uploadedAudio"], function (result: StoredData) {
-      if (result.uploadedAudio) {
-        setAudioSrc(result.uploadedAudio);
-        const audioElement = document.getElementById("audioElement") as HTMLAudioElement;
-        audioElement.load();
-      }
-    });
-  }
-
-  function deleteAudio(): void {
-    chromeStorage.remove("uploadedAudio");
-    setAudioSrc("");
-    setStatusMessage("Custom sound removed. Please refresh Messenger to complete the removal.");
-  }
-
 
   function toggleSetting(setting: keyof SettingsObject) {
     const savedSettings = settingsList;
@@ -101,7 +55,6 @@ export default function App(): JSX.Element {
   }
 
   useEffect(() => {
-    loadAudio();
     loadSettings();
   }, []);
 
@@ -114,7 +67,6 @@ export default function App(): JSX.Element {
         {loadingSettings ? (<p>Loading...</p>) : (
           <>
             <br />
-
             <SettingsItem
               title="Custom Notification Sound"
               desc="Replace the default new message notification sound."
@@ -122,7 +74,6 @@ export default function App(): JSX.Element {
               onChangeFunc={() => toggleSetting('customNotificationSound')}
               svgElement={(<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="bi bi-volume-up-fill" viewBox="0 0 16 16"><path d="M11.536 14.01A8.47 8.47 0 0 0 14.026 8a8.47 8.47 0 0 0-2.49-6.01l-.708.707A7.48 7.48 0 0 1 13.025 8c0 2.071-.84 3.946-2.197 5.303z" /><path d="M10.121 12.596A6.48 6.48 0 0 0 12.025 8a6.48 6.48 0 0 0-1.904-4.596l-.707.707A5.48 5.48 0 0 1 11.025 8a5.48 5.48 0 0 1-1.61 3.89z" /><path d="M8.707 11.182A4.5 4.5 0 0 0 10.025 8a4.5 4.5 0 0 0-1.318-3.182L8 5.525A3.5 3.5 0 0 1 9.025 8 3.5 3.5 0 0 1 8 10.475zM6.717 3.55A.5.5 0 0 1 7 4v8a.5.5 0 0 1-.812.39L3.825 10.5H1.5A.5.5 0 0 1 1 10V6a.5.5 0 0 1 .5-.5h2.325l2.363-1.89a.5.5 0 0 1 .529-.06" /></svg>)}
             />
-
             <SettingsItem
               title="Escape to Close"
               desc="Press the escape key to close the current chat."
@@ -130,7 +81,6 @@ export default function App(): JSX.Element {
               onChangeFunc={() => toggleSetting('escCloseChat')}
               svgElement={(<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="bi bi-x-circle-fill" viewBox="0 0 16 16"><path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293z" /></svg>)}
             />
-
             <SettingsItem
               title="PWA Unread Badges"
               desc="Display badges on the app if installed as a PWA."
@@ -145,12 +95,7 @@ export default function App(): JSX.Element {
       {!loadingSettings && settingsList.customNotificationSound ? (
         <div className="contentCard">
           <h4 className='settingsHeader'>Custom Notification Sound Settings</h4>
-          <SoundSettings 
-            audioSrc={audioSrc}
-            uploadFunction={handleAudioUpload}
-            deleteFunction={deleteAudio}
-            statusMessage={statusMessage}
-          />
+          <SoundSettings />
         </div>
       ) : (<></>)}
 
--- src/mwau-popup/src/components/SoundSettings.tsx
+++ src/mwau-popup/src/components/SoundSettings.tsx
@@ -1,17 +1,62 @@
 import './SoundSettings.css';
+import { chromeStorage } from '../App';
+import { useEffect, useState } from 'react';
 
-interface SoundSettingsProps {
-  audioSrc: string;
-  uploadFunction: () => Promise<void>;
-  deleteFunction: () => void;
-  statusMessage: string;
-}
 
-export default function SoundSettings(props: SoundSettingsProps): JSX.Element {
+export default function SoundSettings(): JSX.Element {
+  const [audioSrc, setAudioSrc] = useState<string>("");
+  const [statusMessage, setStatusMessage] = useState<string>("");
+
+  function uploadAudio(): void {
+    try {
+      const audioFileInput = document.getElementById("audioFileUpload") as HTMLInputElement;
+      const fileUpload: File = audioFileInput.files![0];
+      const reader = new FileReader();
+      reader.onload = function (e: ProgressEvent<FileReader>) {
+        const audioDataURL = e.target!.result as string;
+        chromeStorage.set({ uploadedAudio: audioDataURL },
+          () => {
+            setStatusMessage("Successfully uploaded file. Please refresh Messenger to activate the custom sound.");
+            loadAudio();
+          }
+        );
+      };
+      reader.readAsDataURL(fileUpload);
+    }
+    catch (error: unknown) {
+      if (error instanceof Error) setStatusMessage(error.message);
+    }
+  }
+
+  interface StoredAudio {
+    uploadedAudio?: string;
+  }
+
+  function loadAudio(): void {
+    chromeStorage.get(["uploadedAudio"], function (result: StoredAudio) {
+      if (result.uploadedAudio) {
+        setAudioSrc(result.uploadedAudio);
+        const audioElement = document.getElementById("audioElement") as HTMLAudioElement;
+        audioElement.load();
+      }
+    });
+  }
+
+  function deleteAudio(): void {
+    chromeStorage.remove("uploadedAudio");
+    setAudioSrc("");
+    setStatusMessage("Custom sound removed. Please refresh Messenger to complete the removal.");
+  }
+
+  useEffect(() => {
+    loadAudio();
+  }, []);
+
+
   return (
     <>
       <p>Play current custom sound:
-        <audio src={props.audioSrc} controls id="audioElement">
+        <audio src={audioSrc} controls id="audioElement">
           Your browser does not support the audio tag.
         </audio>
       </p>
@@ -20,18 +65,18 @@ export default function SoundSettings(props: SoundSettingsProps): JSX.Element {
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-plus-circle-fill" viewBox="0 0 16 16">
             <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3z" />
           </svg>
-          <span> {props.audioSrc == "" ? "Upload New" : "Replace Existing"}</span>
+          <span> {audioSrc == "" ? "Upload New" : "Replace Existing"}</span>
         </label>
-        <input type="file" accept="audio/*" id="audioFileUpload" onChange={props.uploadFunction} />
+        <input type="file" accept="audio/*" id="audioFileUpload" onChange={uploadAudio} />
 
-        <button onClick={props.deleteFunction} disabled={props.audioSrc == ""}>
+        <button onClick={deleteAudio} disabled={audioSrc == ""}>
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash3-fill" viewBox="0 0 16 16">
             <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5m-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5M4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06m6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528M8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5" />
           </svg>
           <span> Delete</span>
         </button>
       </p>
-      <p><span>{props.statusMessage}</span></p>
+      <p><span>{statusMessage}</span></p>
     </>
   )
 }
